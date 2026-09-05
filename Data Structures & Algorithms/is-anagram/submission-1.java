4d20bc084d1fc2fca0800d8546e34cbb051184fe
Fady-D <fadydarwish16@gmail.com>
2026-09-04 20:25:43 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count1[s.charAt(i) - 'a']++;
+            count2[t.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(count1, count2)) {
+            return true;
+        }
+
+        return false;
+    }
+}
